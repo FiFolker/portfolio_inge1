375ab71c7516ab6cc13f325a6749fcaab94e6c8a
Elouan Le Merle <elouanle@gmail.com>
2026-05-20 16:11:30 +0200

    Ajout des components de boutons primaires et secondaires

---
+++ src/components/ButtonPrimary.jsx
@@ -0,0 +1,9 @@
+export const ButtonPrimary = ({label, style, onClick}) => {
+    return (
+        <>
+            <button className={"bg-brand-yellow py-2 px-6 text-font-high-emphasis rounded-lg text-center hover:bg-brand-yellow/80 hover:text-font-high-emphasis/80 transition cursor-pointer " + style} onClick={onClick}  >
+                {label}
+            </button>
+        </>
+    )
+}
---
+++ src/components/ButtonSecondary.jsx
@@ -0,0 +1,9 @@
+export const ButtonSecondary = ({label, style, onClick}) => {
+    return (
+        <>
+            <button className={"bg-transparent border-2 border-bg-line py-2 px-6 text-font-high-emphasis rounded-lg text-center hover:text-font-high-emphasis/80 hover:border-bg-line/80 transition cursor-pointer " + style} onClick={onClick}  >
+                {label}
+            </button>
+        </>
+    )
+}
