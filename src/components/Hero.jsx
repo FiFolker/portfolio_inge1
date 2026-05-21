f659b90bc659a53d5b4dcae00a8371315e9c41ef
Elouan Le Merle <elouanle@gmail.com>
2026-05-21 10:42:49 +0200

    fix image de fond (marlene) mise en background et plus dans le Hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,18 +7,6 @@ export const Hero = ({name}) => {
                 <Intro name={name} />
             </div>
 
-            <div className="absolute top-0 right-0 h-full w-1/2 flex justify-end items-start pointer-events-none overflow-hidden">
-
-                <div className="relative h-[85dvh] max-h-[800px] w-full max-w-[800px]">
-
-                    <img
-                        src="/assets/marlene.png"
-                        alt=""
-                        className="absolute top-0 right-0 w-full h-full object-contain object-right select-none"
-                    />
-
-                </div>
-            </div>
         </div>
     );
 };
