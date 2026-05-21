05dec365b51cd394c9a47492d9f30b5c1d08f22c
Elouan Le Merle <elouanle@gmail.com>
2026-05-21 11:56:30 +0200

    extraction du titre sous ligné dans un composant

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,18 +1,15 @@
 import {ProjectItem} from "./ProjectItem.jsx";
+import {TitleUnderlined} from "./TitleUnderlined.jsx";
+
 
 export const Projects = ({projects}) => {
     return (
         <div id="Projects" className="flex-col justify-center items-center space-y-20">
-            <div className="justify-items-center">
-                <h2 className="text-5xl text-bold font-playfair flex-col">
-                    Projects
-                    <div className="mt-1 border-b-brand-yellow border-b-4 w-full"></div>
-                </h2>
-
-            </div>
+            <TitleUnderlined name="Projects" />
             {projects.map((project, i) => {
                 return (
-                    <ProjectItem imageFirst={i % 2 !== 0} title={project.title} description={project.description} image={project.image} link={project.link} key={project.id} />
+                    <ProjectItem imageFirst={i % 2 !== 0} title={project.title} description={project.description}
+                                 image={project.image} link={project.link} key={project.id}/>
                 )
             })}
 
---
+++ src/components/TitleUnderlined.jsx
@@ -0,0 +1,11 @@
+export const TitleUnderlined = ({name}) => {
+    return(
+        <div className="justify-items-center">
+            <h2 className="text-5xl text-bold font-playfair flex-col">
+                {name}
+                <div className="mt-1 border-b-brand-yellow border-b-4 w-full"></div>
+            </h2>
+
+        </div>
+    )
+}
