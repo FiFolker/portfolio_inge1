a514aa25ab7b89b750fda53e1c8c7fbf9628319d
Elouan Le Merle <elouanle@gmail.com>
2026-05-22 09:42:29 +0200

    Mise en place du responsive sur le reste de la page

--- src/App.jsx
+++ src/App.jsx
@@ -12,21 +12,21 @@ function App() {
             id:0,
             title: "Mazespin",
             description:"A Race Game project in Godot with qte",
-            image:"/assets/mazespin.png",
+            image:"/assets/mazespin.webp",
             link:"https://github.com/FiFolker/Mazespin",
         },
         {
             id:1,
             title: "Mini Rogue",
             description:"A roguelike video game written in Java and heavily inspired by the board game of the same name (it could be considered an adaptation as long as I don't change any features or mechanics)",
-            image:"/assets/minirogue.png",
+            image:"/assets/minirogue.webp",
             link:"https://github.com/FiFolker/MiniRogue",
         },
         {
             id:2,
             title: "Rogue Like Shooter",
             description:"Development of a prototype of a 3D rogue-like shooter (like Enter the Gungeon but in 3D). This includes the idea of also testing with the self-learning agents I sent before (the idea of having these artificial agents if to have them play and analyze the difficulty, because the levels should be later generated procedurally)",
-            image:"/assets/roguelike.png",
+            image:"/assets/roguelike.webp",
             link:"https://github.com/FiFolker/Rogue-like-Shooter",
         }
     ]
@@ -34,7 +34,7 @@ function App() {
     return (
         <div className="bg-bg-gray min-h-screen w-full relative overflow-x-hidden profile-img">
 
-            <div className="absolute top-0 left-0 w-full px-20 z-50 mt-4 ">
+            <div className="absolute top-0 left-0 w-full px-6 sm:px-10 md:px-20 z-50 mt-4">
                 <Navbar name={name} />
             </div>
 
--- src/components/ButtonSecondary.jsx
+++ src/components/ButtonSecondary.jsx
@@ -1,7 +1,7 @@
 export const ButtonSecondary = ({label, style, onClick}) => {
     return (
         <>
-            <button className={"text-lg bg-transparent border-2 border-bg-line py-2 px-6 text-font-high-emphasis rounded-lg text-center hover:text-font-high-emphasis/80 hover:border-bg-line/80 transition cursor-pointer " + style} onClick={onClick}  >
+            <button className={"text-base sm:text-lg bg-transparent border-2 border-bg-line py-2 px-5 sm:px-6 text-font-high-emphasis rounded-lg text-center hover:text-font-high-emphasis/80 hover:border-bg-line/80 transition cursor-pointer " + style} onClick={onClick}  >
                 {label}
             </button>
         </>
