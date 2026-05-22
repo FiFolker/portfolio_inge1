a122392c084615f69a9175cccd8e0a9e97f894b3
Elouan Le Merle <elouanle@gmail.com>
2026-05-22 09:41:56 +0200

    Mise en place du responsive sur le Hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,8 +2,8 @@ import {Intro} from "./Intro.jsx";
 
 export const Hero = ({name}) => {
     return (
-        <div id="About" className="relative min-h-screen w-full flex items-center">
-            <div className="pl-20 z-10 max-w-2xl">
+        <div id="About" className="relative w-full flex items-start sm:items-center pt-20 sm:pt-0 pb-6 sm:pb-0 min-h-[55vh] sm:min-h-screen">
+            <div className="px-4 sm:px-10 md:px-20 z-10 max-w-2xl mx-auto">
                 <Intro name={name} />
             </div>
 
