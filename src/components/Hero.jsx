2c3b9ac8302c97e31bb1bcc2cf1f9d5b15cf44e0
Elouan Le Merle <elouanle@gmail.com>
2026-05-22 09:53:42 +0200

    Image apparaît et disparaît responsivement

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,7 +3,7 @@ import {Intro} from "./Intro.jsx";
 export const Hero = ({name}) => {
     return (
         <div id="About" className="relative w-full flex items-start sm:items-center pt-20 sm:pt-0 pb-6 sm:pb-0 min-h-[55vh] sm:min-h-screen">
-            <div className="px-4 sm:px-10 md:px-20 z-10 max-w-2xl mx-auto">
+            <div className="px-4 sm:px-10 md:px-20 z-10 max-w-2xl mx-auto sm:mx-0">
                 <Intro name={name} />
             </div>
 
