70abcc81b05079404984cf7eb007dd1ea307a109
Elouan Le Merle <elouanle@gmail.com>
2026-05-22 15:16:38 +0200

    Polish du responsive des items de projects

--- src/components/ProjectItem.jsx
+++ src/components/ProjectItem.jsx
@@ -1,19 +1,19 @@
 export const ProjectItem = ({ imageFirst=false ,title, description, image, link }) => {
     return (
-        <div className="bg-bg-white rounded-3xl overflow-hidden flex flex-col md:flex-row w-full max-w-5xl shadow-[0_8px_30px_rgb(0,0,0,0.06)] mx-auto">
+        <div className="bg-bg-white rounded-3xl overflow-hidden flex flex-col md:flex-row w-full max-w-5xl shadow-[0_8px_30px_rgb(0,0,0,0.06)] mx-auto md:h-[524px]">
 
 
             {imageFirst &&
-                <div className="w-full md:w-1/2 min-h-[300px] md:min-h-full">
+                <div className="w-full md:w-1/2 h-[300px] md:h-full">
                 <img
                 src={image}
                 alt={`Aperçu du projet ${title}`}
-                className="w-full h-full object-cover"
+                className="w-full h-full object-cover object-top"
                 />
                 </div>
             }
 
-            <div className="w-full md:w-1/2 p-6 sm:p-10 md:p-14 lg:p-16 flex flex-col justify-center">
+            <div className="w-full md:w-1/2 p-6 sm:p-10 md:p-14 lg:p-16 flex flex-col justify-center md:h-full">
                 <h3 className="font-playfair text-font-high-emphasis text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-bold mb-4 sm:mb-6">
                     {title}
                 </h3>
@@ -32,11 +32,11 @@ export const ProjectItem = ({ imageFirst=false ,title, description, image, link 
             </div>
 
             {!imageFirst &&
-                <div className="w-full md:w-1/2 min-h-[300px] md:min-h-full">
+                <div className="w-full md:w-1/2 h-[300px] md:h-full">
                     <img
                         src={image}
                         alt={`Aperçu du projet ${title}`}
-                        className="w-full h-full object-cover"
+                        className="w-full h-full object-cover object-bottom"
                     />
                 </div>
             }
